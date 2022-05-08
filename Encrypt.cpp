2a66d5f5c847cbb73528f73513ec50136ea7c622
brianchennn <ny40ny40ny.cs07@nctu.edu.tw>
2022-05-09 02:39:59 +0800

    TODO: convolution, shuffle, jpg2Array

--- Encrypt.cpp
+++ Encrypt.cpp
@@ -1,10 +1,111 @@
 #include <iostream>
+#include <fstream>
+#include <cstring>
 
 using namespace std;
 
+bool isCoprime(short a, short b){
+    while(b != 0){
 
+        short tmp = a % b;
+        a = b;
+        b = tmp;
+        if(b == 1){
+            return true;
+        } 
+    }
+    return false;
+}
+
+
+short chooseCoprime(short p, short row){
+    short a = (p/3 + row) % p;
+    while(1){
+        if(isCoprime(p,a)){
+            return a;
+        }
+        a = (a+1) % p;
+    }
+    return a;
+}
+
+short **Transpose(short **A, int row, int col){
+    short **newp = new short*[col];
+    for (short i = 0; i < col; i++) {
+        newp[i] = new short[row]{0};
+        for(short j = 0; j < row; j++){
+            newp[i][j] = A[j][i];
+        }
+    }
+    for(int i = 0 ; i < row ; i++){
+        delete A[i];
+    }
+    
+    return newp;
+}
+
+void shiftRow(short **A, int row, int col){
+
+    for(short i = 0 ; i < row ; i++){
+        short a = chooseCoprime(col, i);
+        short b = i; 
+        short *newRow = new short[col];
+        for(int j = 0 ; j < col ; j++){
+            short f = (a * j + b)%col; 
+            newRow[f] = A[i][j];
+        }
+        delete A[i];
+        A[i] = newRow;
+    }
+}
+
+void convolution(int **A, int row, int col){
+    /*TODO*/
+}
+void shuffle(int **A, int row, int col){
+    /*TODO*/
+}
 
 int main()
 {
+    short row,col;
+    //string filename("CowRol")
+    ifstream f,fR,fG,fB;
+    f.open("RowCol.txt");
+    f >> row >> col;
+    f.close();
+    fR.open("R.txt");
+    fG.open("G.txt");
+    fB.open("B.txt");
+    
+    short **R = new short*[row];
+    short **G = new short*[row];
+    short **B = new short*[row];
+    for (short i = 0; i < row; i++) {
+        R[i] = new short[col]{0};
+        G[i] = new short[col]{0};
+        B[i] = new short[col]{0};
+    }
+    cout << row << col <<endl;
+    for(short i = 0; i < row ; i++){
+        for(short j = 0 ; j < col; j++){
+            fR >> R[i][j];
+            fG >> G[i][j];
+            fB >> B[i][j];
+        }
+    }
+    fR.close();
+    fG.close();
+    fB.close();
+    shiftRow(R, row, col);
+    B = Transpose(B, row, col);
+    shiftRow(B, col, row);
 
+    /*
+    for(short i = 0; i < row ; i++){
+        for(short j = 0 ; j < col; j++){
+            cout << B[i][j] << " ";
+        }
+        cout << endl;
+    }*/
 }
--- jpgTo2Darray.py
+++ jpgTo2Darray.py
@@ -1,47 +1,30 @@
+# https://stackoverflow.com/questions/27026866/convert-an-image-to-2d-array-in-python
 from PIL import Image
 import numpy as np
 import os
 
 im = Image.open("ayaka.jpg")
 col,row =  im.size
-data = np.zeros((row*col, 5))
-R = np.zeros((row,col)).astype(int)
-G = np.zeros((row,col))
-B = np.zeros((row,col))
 pixels = im.load()
 
-os.remove('R.txt')
-os.remove('G.txt')
-os.remove('B.txt')
-f1 = open("R.txt","a")
-f2 = open("G.txt","a")
-f3 = open("B.txt","a")
+
+f1 = open("R.txt","w")
+f2 = open("G.txt","w")
+f3 = open("B.txt","w")
+f4 = open("RowCol.txt","w")
+
+f4.write("%d %d"%(row,col))
 
 for i in range(row):
     for j in range(col):
         r,g,b =  pixels[i,j]
-        data[i*col + j,:] = r,g,b,i,j
-        #f1.write("{0:b} ".format(r))
-        #f2.write("{0:b} ".format(g))
-        #f3.write("{0:b} ".format(b))
+
         f1.write("%d "%r)
         f2.write("%d "%g)
         f3.write("%d "%b)
-        R[i][j] = r
-        G[i][j] = g
-        B[i][j] = b
-    f1.write("\n")
-    f2.write("\n")
-    f3.write("\n")
 f1.close()
 f2.close()
 f3.close()
-'''
-for row in :
-    for ele in row:
-        print("{0:b}".format(ele))
-        f1.write("{0:b}".format(ele))
-        #f.write("")
-    f.write("\n")
-f.close()'''
+
+
 
